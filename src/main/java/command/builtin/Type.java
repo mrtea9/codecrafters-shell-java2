cb24ab2aa15a72a2179c019c620c84a19aef6e47
mrtea9 <ionmeriacri111@gmail.com>
2024-12-04 15:25:13 +0200

    type executable

--- src/main/java/command/builtin/Type.java
+++ src/main/java/command/builtin/Type.java
@@ -17,10 +17,9 @@ public enum Type implements Builtin {
         if (command instanceof Builtin) {
             System.out.println("%s is a shell builtin".formatted(program));
         } else if (command instanceof Executable(final var path)) {
-            System.out.println("%s is %s".formatted(program, path))
+            System.out.println("%s is %s".formatted(program, path));
         } else {
             System.out.println("%s not found".formatted(program));
         }
     }
-
 }
