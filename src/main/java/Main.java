9b838e627e5f55f1c7300d21267615de4de526ad
mrtea9 <ionmeriacri111@gmail.com>
2024-12-04 15:42:20 +0200

    single quotes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,7 +27,7 @@ public class Main {
     }
 
     public static void eval(Shell shell, String line) {
-        final var arguments = line.split("(?<=')\\\\s+(?=')");
+        final var arguments = line.split("");
         final var program = arguments[0];
 
         final var command = shell.find(program);
