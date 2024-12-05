995df37b5616cd04a14007503d904eb2dad3bcf5
mrtea9 <ionmeriacri111@gmail.com>
2024-12-05 10:57:37 +0200

    blackslash

--- src/main/java/shell/Shell.java
+++ src/main/java/shell/Shell.java
@@ -68,6 +68,7 @@ public class Shell {
         arguments.add(command);
         arguments.addAll(commandArguments);
 
+
         String[] result = new String[arguments.size()];
 
         return arguments.toArray(result);
